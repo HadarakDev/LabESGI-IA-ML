2fb07578d598272cbd4bf195117f0ce26d25a581
bergeron.florian@gmail.com <bergeron.florian@gmail.com>
2020-01-09 18:59:53 +0100

    [UP] - Q_Learning solution's problem

--- TP2_Q_Learning/q_learning.py
+++ TP2_Q_Learning/q_learning.py
@@ -4,8 +4,8 @@ import pygame
 import numpy as np
 import random
 
-from TP2_Q_Learning.config import score_map, item_map, actions, position_map, action_text
-from TP2_Q_Learning.fix_me import *
+from config import score_map, item_map, actions, position_map, action_text
+from fix_me import *
 
 #### GRAPHICAL PYGAME FUNCTION
 def display_board(win, item_map_edit, posX, posY, background_img):
@@ -146,5 +146,4 @@ if __name__ == "__main__":
             display_info(x, y, q_table, reward, epochs, action, epsilon)
 
             # Uncomment to wait after each action
-            #sleep(0.5)
-
+            # sleep(0.1)
