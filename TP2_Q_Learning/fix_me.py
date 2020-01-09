fdb09e1345888a550b4c74e754efa2e9bae64f1a
Clément Depraz <clem.depraz@gmail.com>
2020-01-09 15:39:40 +0100

    Fix fix.me file
    Use arbitrary threshold values
    Init gitignore
    Fix import

--- TP2_Q_Learning/fix_me.py
+++ TP2_Q_Learning/fix_me.py
@@ -4,23 +4,23 @@ import random
 ############ EDIT ABOVE FUNCTIONS ####
 
 def create_Q_table():  # create q table with zeros and size
-    q_table = np.zeros('''FIX ME''')  # number of states, number of actions
+    q_table = np.zeros((9, 4))  # number of states, number of actions
     return q_table
 
 def update(q_table, state, action, r, Gamma, stateNext, actionNext):
     # use formula to update q_table
-    q_table[state][action] = '''FIX ME'''
+    q_table[state][action] += 0.85 * (r + Gamma * max(q_table[stateNext]) - q_table[state][action])
     return q_table
 
 def choose_action(state, q_table, epsilon):
-    if '''FIX ME''': #  take a random number and compare to exploratio rate
-        action = random.randint('''FIX ME''')  # choose a random action
+    if random.uniform(0, 1) < epsilon: #  take a random number and compare to exploratio rate
+        action = random.randint(0, 3)  # choose a random action
     else:
-        action = np.argmax('''FIX ME''') # choose the best action for a state
+        action = np.argmax(q_table[state,:])
     return action
 
 def reduce_epsilon(epsilon):
 
-    epsilon = epsilon * '''FIX ME'''  # reduce epsilon after each step
-    epsilon = max('''FIX ME''') # set a minimum value for epsilon
+    epsilon = epsilon * 0.995  # reduce epsilon after each step
+    epsilon = max(epsilon, 0.5) # set a minimum value for epsilon
     return epsilon
